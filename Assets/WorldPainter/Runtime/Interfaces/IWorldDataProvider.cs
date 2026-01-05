4d21f9b2852d7c9d45aa1d24ab7c63473c8c73c0
Yackov <banzai.davai@gmail.com>
2026-01-05 17:59:12 +0500

    #7 1.2

---
+++ Assets/WorldPainter/Runtime/Interfaces/IWorldDataProvider.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace WorldPainter.Runtime.Interfaces
+{
+    public interface IWorldDataProvider
+    {
+        string GetTileIdAt(Vector2Int position);
+        void SetTileIdAt(Vector2Int position, string tileId);
+    }
+}
