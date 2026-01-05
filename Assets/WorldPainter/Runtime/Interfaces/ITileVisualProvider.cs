b49eb5864bb0aacee62156fee12c31668486f628
Yackov <banzai.davai@gmail.com>
2026-01-05 18:31:38 +0500

    #9 2.1

---
+++ Assets/WorldPainter/Runtime/Interfaces/ITileVisualProvider.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace WorldPainter.Runtime.Interfaces
+{
+    public interface ITileVisualProvider
+    {
+        GameObject GetTileVisual(string tileId, Vector3 position);
+    }
+}
