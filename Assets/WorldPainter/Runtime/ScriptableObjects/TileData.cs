6df01b043b879c01c3f72244b2e49c996d5dad0f
Yackov <banzai.davai@gmail.com>
2026-01-05 17:57:57 +0500

    #6 1.1

---
+++ Assets/WorldPainter/Runtime/ScriptableObjects/TileData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace WorldPainter.Runtime.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "Tile Data", menuName = "WorldPainter/TileData", order = 0)]
+    public class TileData : ScriptableObject
+    {
+        [SerializeField] private string displayName;
+            
+        public string TileId { get; }
+        public string DisplayName => displayName;
+
+        public void OnValidate()
+        {
+            
+        }
+    }
+}
